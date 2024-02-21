55b07bc2205713a256a560bed20e87438d2f9e19
AtomicAnt27 <pedrodafonsecaschwertner@gmail.com>
2024-02-21 11:28:30 -0300

    Cardapio do Ditto

---
+++ Activity03/Cardapio/cardapio_script.js
@@ -0,0 +1,10 @@
+
+function muda_cardapio(n) {
+    var cardapios = document.querySelectorAll('.cardapio');
+
+    cardapios.forEach(cardapio => {
+        cardapio.classList.add('hidden');
+    });
+
+    cardapios[n].classList.remove('hidden');
+}
