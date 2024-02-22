39d237a82c58e62a2331260621a7a7764228843d
AtomicAnt27 <pedrodafonsecaschwertner@gmail.com>
2024-02-22 11:35:26 -0300

    responsive menu

--- Activity03/Cardapio/cardapio_script.js
+++ Activity03/Cardapio/cardapio_script.js
@@ -8,3 +8,14 @@ function muda_cardapio(n) {
 
     cardapios[n].classList.remove('hidden');
 }
+
+
+function aparece_cardapio(n) {
+    var cardapios = document.querySelectorAll('.content');
+
+    cardapios.forEach(cardapio => {
+        cardapio.classList.add('hidden');
+    });
+
+    cardapios[n].classList.remove('hidden');
+}
