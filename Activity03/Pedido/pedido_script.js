5b1b7ec56e16c62cc8d5c39fe0ef4203d8b3b4d4
AtomicAnt27 <pedrodafonsecaschwertner@gmail.com>
2024-02-23 08:50:51 -0300

    pedido mt foda

---
+++ Activity03/Pedido/pedido_script.js
@@ -0,0 +1,22 @@
+function comecar(){
+    var name = document.getElementById("exampleFormControlInput1").value;
+
+    show(name);
+}
+
+function show(name){
+    document.getElementById("nomePrint").textContent = name;
+    var div = document.getElementById("selects-div");
+    if(name && name.trim() !== ""){
+        div.style.display = "block";
+        document.getElementById("exampleFormControlInput1").style.background = "none";
+    }else{
+        document.getElementById("error").style.display = "flex";
+        div.style.display = "none";
+    }
+}
+
+function fechar(){
+    document.getElementById("error").style.display = "none";
+    document.getElementById("exampleFormControlInput1").style.background = "lightpink";
+}
