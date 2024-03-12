4e25c801885f954beba16b288df1096cb413b5b1
AtomicAnt27 <pedrodafonsecaschwertner@gmail.com>
2024-03-12 09:41:15 -0300

    kjkjbjb

--- Table to Excel/script.js
+++ Table to Excel/script.js
@@ -1,7 +1,7 @@
 function adicionarItem() {
-    var nome = document.getElementById("nome");
-    var valor = document.getElementById("valor");
-    var quantidade = document.getElementById("quantidade");
+    var nome = document.getElementById("nome").value;
+    var valor = document.getElementById("valor").value;
+    var quantidade = document.getElementById("quantidade").value;
 
     if(!nome || !valor || !quantidade){
         alert("Preencha todos os campos!")
@@ -9,4 +9,24 @@ function adicionarItem() {
     }
 
     var tabela = document.getElementById("tabela").getElementsByTagName("tbody")[0];
+    var novaLinha = tabela.insertRow(tabela.rows.length);
+
+    var celulaNome = novaLinha.insertCell(0)
+    var celulaValor = novaLinha.insertCell(1)
+    var celulaQuantidade = novaLinha.insertCell(2)
+
+    celulaNome.innerHTML = nome;
+    celulaValor.innerHTML = valor;
+    celulaQuantidade.innerHTML = quantidade;
+
+    document.getElementById("nome").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("quantidade").value = "";
+}
+
+function exportarParaExcel(){
+    var tabela = document.getElementById("tabela");
+    var nomeArquivo = "tabela_de_produtos.xlsx";
+    var wb = XLSX.utils.table_to_book(tabela, {sheet: "Tabela de produtos"})
+    XLSX.writeFile(wb, nomeArquivo)
 }
